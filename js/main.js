d9f818efbb6c7ec48589b13fd0d394d6e174f486
adelinashahaj <adelinarucaj@icloud.com>
2023-03-23 18:01:02 +0100

    separato il v-for e v-if in 2 tag separati

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ createApp({
   mounted() {
    
   for(let i = 0; i < 10; i++){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail?')
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then( (response) => {
             this.randomMail.push(response.data.response);
         }
