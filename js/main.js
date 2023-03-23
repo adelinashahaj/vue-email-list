8de6f9362cc2483bfa1ce75c0df78eaf2a50e618
adelinashahaj <adelinarucaj@icloud.com>
2023-03-23 14:38:37 +0100

    es + bonus

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      randomMail: []
+    }
+  },
+  mounted() {
+   
+  for(let i = 0; i < 10; i++){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail?')
+    .then( (response) => {
+            this.randomMail.push(response.data.response);
+        }
+    );
+  }
+    
+    
+
+  }
+}).mount('#app')
